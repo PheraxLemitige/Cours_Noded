30064e0c26b785ef3f2d72ff65067025f31f2c48
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2023-03-09 18:47:02 +0100

    creation du chemin changeStatue

--- tp2/src/routes/watchlists.js
+++ tp2/src/routes/watchlists.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addMovie, getWatchListUser, getWatchList } = require("../controllers/watchlists");
+const { addMovie, getWatchListUser, getWatchList, changeStatue } = require("../controllers/watchlists");
 
 router.post("/create", addMovie);
 router.get("/getWatchlistUser", getWatchListUser);
 router.get("/getWatchlist", getWatchList);
+router.post("/changeStatue", changeStatue);
 
 module.exports = router;
